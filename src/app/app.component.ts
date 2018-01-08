74af87b6d801fd0bcc645e5a4c6e0fb164129ea1
CtrlNull <raphael@fonseca.tech>
2018-01-07 20:49:04 -0800

    fixed rxjs/Rx blacklist bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http, Response } from '@angular/http';
+
+import { AppService } from './app.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  greetings = '';
   title = 'app';
+
+  constructor(private _appService: AppService) { }
+
+  ngOnInit(): void {
+    this._appService.sayHello()
+    .subscribe(
+      result => {
+        this.greetings = result;
+      }
+    );
+  }
 }
